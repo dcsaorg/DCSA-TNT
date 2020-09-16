c2b040b548f85830cc49b831af43cf6e8ad58bf6
Henrik Helmø Larsen <hhl@asseco.dk>
2020-09-16 14:27:28 +0200

    Minor updates

---
+++ datamodel/initdb.d/04_dcsa_tnt_v2_0.sql
@@ -0,0 +1,131 @@
+-- A script to initialize the tables relevant for the DCSA TNT interface v2.0
+
+\connect dcsa_openapi
+
+DROP TABLE IF EXISTS dcsa_v2_0.event CASCADE;
+CREATE TABLE dcsa_v2_0.event (
+    event_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    event_type text NOT NULL,
+    event_classifier_code varchar(3) NOT NULL,
+    event_date_time timestamp with time zone NOT NULL,
+    event_type_code varchar(4) NOT NULL,
+    transport_call_id uuid NOT NULL
+);
+
+DROP TABLE IF EXISTS dcsa_v2_0.equipment_event CASCADE;
+CREATE TABLE dcsa_v2_0.equipment_event (
+    equipment_reference varchar(15),
+    empty_indicator_code text NOT NULL
+) INHERITS (dcsa_v2_0.event);
+
+DROP TABLE IF EXISTS dcsa_v2_0.shipment_event CASCADE;
+CREATE TABLE dcsa_v2_0.shipment_event (
+    shipment_information_type_code varchar(3) NOT NULL
+) INHERITS (dcsa_v2_0.event);
+
+DROP TABLE IF EXISTS dcsa_v2_0.transport_event CASCADE;
+CREATE TABLE dcsa_v2_0.transport_event (
+    delay_reason_code varchar(3),
+    vessel_schedule_change_remark varchar(250)
+) INHERITS (dcsa_v2_0.event);
+
+DROP TABLE IF EXISTS dcsa_v2_0.event_subscription CASCADE;
+CREATE TABLE dcsa_v2_0.event_subscription (
+    subscription_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    callback_url text NOT NULL,
+    event_type text, --This field must be able to contain multiple event types. Currently it does not.
+    booking_reference varchar(35),
+    transport_document_id varchar(20),
+    transport_document_type text,
+    equipment_reference varchar(15),
+    schedule_id uuid NOT NULL,
+    transport_call_id uuid NOT NULL
+    );
+
+
+-- Aggregated table containing all events
+DROP VIEW IF EXISTS dcsa_v2_0.aggregated_events CASCADE;
+CREATE VIEW dcsa_v2_0.aggregated_events AS
+ SELECT transport_event.event_id,
+    transport_event.event_type,
+    transport_event.event_classifier_code,
+    transport_event.event_type_code,
+    transport_event.event_date_time,
+    transport_event.transport_call_id,
+    transport_event.delay_reason_code,
+    transport_event.vessel_schedule_change_remark,
+    NULL::text AS shipment_information_type_code,
+    NULL::text AS equipment_reference,
+    NULL::text AS empty_indicator_code
+   FROM dcsa_v2_0.transport_event
+UNION
+ SELECT shipment_event.event_id,
+    shipment_event.event_type,
+    shipment_event.event_classifier_code,
+    shipment_event.event_type_code,
+    shipment_event.event_date_time,
+    shipment_event.transport_call_id,
+    NULL::text AS delay_reason_code,
+    NULL:: text AS vessel_schedule_change_remark,
+    shipment_event.shipment_information_type_code,
+    NULL::text AS equipment_reference,
+    NULL::text AS empty_indicator_code
+   FROM dcsa_v2_0.shipment_event
+UNION
+ SELECT equipment_event.event_id,
+    equipment_event.event_type,
+    equipment_event.event_classifier_code,
+    equipment_event.event_type_code,
+    equipment_event.event_date_time,
+    equipment_event.transport_call_id,
+    NULL::text AS delay_reason_code,
+    NULL:: text AS vessel_schedule_change_remark,
+    NULL::text AS shipment_information_type_code,
+    equipment_event.equipment_reference,
+    equipment_event.empty_indicator_code
+   FROM dcsa_v2_0.equipment_event;
+
+--Helper table in order to filter Events on schedule_id
+DROP TABLE IF EXISTS dcsa_v2_0.schedule CASCADE;
+CREATE TABLE dcsa_v2_0.schedule (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    vessel_operator_carrier_code varchar(10) NOT NULL,
+    vessel_operator_carrier_code_list_provider text NOT NULL,
+    vessel_partner_carrier_code text NOT NULL,
+    vessel_partner_carrier_code_list_provider text,
+    start_date date,
+    date_range interval
+);
+
+DROP TABLE IF EXISTS dcsa_v2_0.transport_call CASCADE;
+CREATE TABLE dcsa_v2_0.transport_call (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    schedule_id uuid NOT NULL,
+    carrier_service_code text,
+    vessel_imo_number varchar(7),
+    vessel_name varchar(35),
+    carrier_voyage_number varchar(50) NOT NULL,
+    un_location_code varchar(5) NOT NULL,
+    un_location_name varchar(70),
+    transport_call_number integer,
+    facility_type_code varchar(4) NOT NULL,
+    facility_code varchar(11) NOT NULL,
+    other_facility varchar(50)
+);
+
+-- Helper table in order to filter Events on equipment-reference
+DROP TABLE IF EXISTS dcsa_v2_0.shipment CASCADE;
+CREATE TABLE dcsa_v2_0.shipment (
+    id uuid NOT NULL,
+    booking_reference text, -- The identifier for a shipment, which is issued by and unique within each of the carriers.
+    booking_datetime timestamp, -- The date and time of the booking request.
+    transport_document_id UUID, -- Transport Document ID is an identifier that links to a shipment. Bill of lading is the legal document issued to the customer which confirms the carrier's receipt of the cargo from the customer acknowledging goods being shipped and specifying the terms of delivery.
+    transport_document_type_code text,
+    shipper_name varchar(50), -- The name of the shipper, who requested the booking
+    consignee_name varchar(50), -- The name of the consignee
+    collection_origin varchar(250), -- The location through which the shipment originates. It can be defined as a UN Location Code value or an address. The customer (shipper) needs to place a booking in order to ship the cargo (commodity) from an origin to destination. This attribute specifies the location of the origin.
+    collection_dateTime timestamp, -- The date and the time that the shipment items need to be collected from the origin.
+    delivery_destination varchar(250), -- The location to which the shipment is destined. It can be defined as a UN Location Code value or an address. The customer (shipper) needs to place a booking in order to ship the cargo (commodity) from an origin to destination. This attribute specifies the location of the destination. Also known as 'place of carrier delivery'.
+    delivery_datetime timestamp , -- The date (and when possible time) that the shipment items need to be delivered to the destination.
+    carrier_code varchar(10) -- The Carrier Code represents a concatenation of the Code List Provider Code and the Code List Provider. A hyphen is used between the two codes. The unique carrier identifier is sourced from either the NMFTA SCAC codes list or the SMDG Master Liner codes list.
+    );
--- datamodel/testdata.d/05_test_data.sql
+++ datamodel/testdata.d/05_test_data.sql
@@ -1,6 +1,8 @@
 \connect dcsa_openapi
 
-INSERT INTO dcsa_v1_1.shipment_event (
+--- Insert data into v1_2 model ---
+
+INSERT INTO dcsa_v1_2.shipment_event (
     event_classifier_code,
     event_type,
     event_date_time,
@@ -8,10 +10,22 @@ INSERT INTO dcsa_v1_1.shipment_event (
     event_type_code,
     shipment_id,
     shipment_information_type_code
-    )
-    VALUES ('PLN', 'SHIPMENT', TO_DATE('2020/07/15 13:14:15', 'yyyy/mm/dd hh24:mi:ss'), 'e48f2bc0-c746-11ea-a3ff-db48243a89f4', 'DEPA', uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'), 'Some type code text');
+) VALUES (
+    'PLN', 'SHIPMENT', TO_DATE('2020/07/15 13:14:15', 'yyyy/mm/dd hh24:mi:ss'), 'e48f2bc0-c746-11ea-a3ff-db48243a89f4', 'DEPA', uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'), 'Some type code text'
+);
 
-INSERT INTO "dcsa_v1_1".equipment_event(
+INSERT INTO "dcsa_v1_2".shipment_event (
+    event_classifier_code,
+    event_type,
+    event_date_time,
+    event_type_code,
+    shipment_id,
+    shipment_information_type_code
+) VALUES (
+    'PLN', 'SHIPMENT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'ARRI', uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'), 'shipment_type_code'
+);
+
+INSERT INTO "dcsa_v1_2".equipment_event (
 	event_classifier_code,
 	event_type,
 	event_date_time,
@@ -22,10 +36,11 @@ INSERT INTO "dcsa_v1_1".equipment_event(
 	facility_code,
 	other_facility,
 	empty_indicator_code
-	)
-	VALUES ('ACT', 'EQUIPMENT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'ARRI', 'equipref3453', 'HYDRO', 'LA', 'LAHO', '', 'EMPTY');
+) VALUES (
+    'ACT', 'EQUIPMENT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'ARRI', 'equipref3453', 'HYDRO', 'LA', 'LAHO', '', 'EMPTY'
+);
 
-INSERT INTO "dcsa_v1_1".transport_equipment_event(
+INSERT INTO "dcsa_v1_2".transport_equipment_event (
 	event_classifier_code,
 	event_type,
 	event_date_time,
@@ -39,10 +54,11 @@ INSERT INTO "dcsa_v1_1".transport_equipment_event(
 	transport_reference,
 	transport_leg_reference,
 	mode_of_transport_code
-	)
-	VALUES ('PLN', 'TRANSPORTEQUIPMENT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'ARRI', 'eqref123', 'factory', 'CPH', '2', 'no', 'LADEN', 'ref123', 'legref', '7');
+) VALUES (
+    'PLN', 'TRANSPORTEQUIPMENT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'ARRI', 'eqref123', 'factory', 'CPH', '2', 'no', 'LADEN', 'ref123', 'legref', '7'
+);
 
-INSERT INTO "dcsa_v1_1".transport_event(
+INSERT INTO "dcsa_v1_2".transport_event (
 	event_classifier_code,
 	event_type,
 	event_date_time,
@@ -54,18 +70,93 @@ INSERT INTO "dcsa_v1_1".transport_event(
 	facility_code,
 	other_facility,
 	mode_of_transport_code
-	)
-	VALUES ('ACT', 'TRANSPORT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'),  'DEPA', 'transportref123', 'legreference234', 'coal', 'PORT', 'NYC', 'no', '2');
+) VALUES (
+    'ACT', 'TRANSPORT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'),  'DEPA', 'transportref123', 'legreference234', 'coal', 'PORT', 'NYC', 'no', '2'
+);
 
-INSERT INTO "dcsa_v1_1".shipment_event(
-    event_classifier_code,
+INSERT INTO dcsa_v1_2.event_subscription (
+    callback_url,
     event_type,
-    event_date_time,
-    event_type_code,
-    shipment_id,
-    shipment_information_type_code
-    )
-    VALUES ('PLN', 'SHIPMENT', TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'ARRI', uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'), 'shipment_type_code');	
+    booking_reference,
+    bill_of_lading_number,
+    equipment_reference
+) VALUES (
+    'http://localhost:4567/webhook/receive', '', '', '', ''
+);
+
+INSERT INTO dcsa_v1_2.event_subscription (
+    callback_url,
+    event_type,
+    booking_reference,
+    bill_of_lading_number,
+    equipment_reference
+) VALUES (
+    'http://localhost:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', ''
+);
+
+INSERT INTO dcsa_v1_2.event_subscription (
+    callback_url,
+    event_type,
+    booking_reference,
+    bill_of_lading_number,
+    equipment_reference
+) VALUES (
+    'http://172.17.0.1:4567/webhook/receive', '', '', '', ''
+);
+
+INSERT INTO dcsa_v1_2.shipment (
+    id,
+    booking_reference,
+    booking_datetime,
+    bill_of_lading_number,
+    shipper_name,
+    consignee_name,
+    collection_origin,
+    collection_dateTime,
+    delivery_destination,
+    delivery_datetime,
+    carrier_code
+) VALUES (
+    uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'),
+    'BR1239719871',
+    DATE '2020-03-07',
+    'BL32147109',
+    'Magic Wands Inc.',
+    'The Mage Guild',
+    '5, Mountain Road, The land beyond the sea',
+    DATE '2020-03-10',
+    '125, Valley Street, The land nearby',
+    DATE '2020-04-10',
+    'SMDG-ZIM'
+);
+
+INSERT INTO dcsa_v1_2.shipment (
+    id,
+    booking_reference,
+    booking_datetime,
+    bill_of_lading_number,
+    shipper_name,
+    consignee_name,
+    collection_origin,
+    collection_dateTime,
+    delivery_destination,
+    delivery_datetime,
+    carrier_code
+) VALUES (
+    uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea2'),
+    'BR1239719872',
+    DATE '2021-03-07',
+    'AA32147122',
+    'Unicorn Inc.',
+    'Long horn',
+    '5, Lake View, By the lake',
+    DATE '2021-05-23',
+    '125, Wall St, Next door',
+    DATE '2020-08-01',
+    'SMDG-ZIM'
+);
+
+--- Insert data into v2_0 model ---
 
 INSERT INTO dcsa_v2_0.schedule (
     id,
@@ -75,8 +166,7 @@ INSERT INTO dcsa_v2_0.schedule (
     date_range,
     vessel_operator_carrier_code_list_provider,
     vessel_partner_carrier_code_list_provider
-)
-VALUES (
+) VALUES (
     uuid('35b7b170-c751-11ea-a305-7b347bb9119f'),
     'ZIM',
     'MSK',
@@ -99,8 +189,7 @@ INSERT INTO dcsa_v2_0.transport_call (
     facility_type_code,
     facility_code,
     other_facility
-    )
-VALUES (
+) VALUES (
     uuid('8b64d20b-523b-4491-b2e5-32cfa5174eee'),
     uuid('35b7b170-c751-11ea-a305-7b347bb9119f'),
     'Y6S',
@@ -112,57 +201,29 @@ VALUES (
     3,
     'TERM',
     'ITGOAASEA',
-    NULL);
+    NULL
+);
 
 INSERT INTO dcsa_v2_0.shipment_event (
-    id,
-    shipment_id,
+    event_id,
     event_classifier_code,
     event_type,
     event_date_time,
     event_type_code,
-    shipment_information_type_code
-    )
-VALUES (
+    shipment_information_type_code,
+    transport_call_id
+) VALUES (
     uuid('784871e7-c9cd-4f59-8d88-2e033fa799a1'),
-    uuid('6e2d856c-d871-11ea-a630-03e5334d1800'),
     'PLN',
     'SHIPMENT',
     '2020-07-15',
     'DEPA',
-    'WTF');
+    'WTF',
+    uuid('8b64d20b-523b-4491-b2e5-32cfa5174eee')
+);
 
-
-INSERT INTO dcsa_v1_1.event_subscription(
-    callback_url,
-    event_type,
-    booking_reference,
-    bill_of_lading_number,
-    equipment_reference
-)
-    VALUES ('http://localhost:4567/webhook/receive', '', '', '', '');
-
-    INSERT INTO dcsa_v1_1.event_subscription(
-    callback_url,
-    event_type,
-    booking_reference,
-    bill_of_lading_number,
-    equipment_reference
-)
-    VALUES ('http://localhost:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
-
-INSERT INTO dcsa_v1_1.event_subscription(
-    callback_url,
-    event_type,
-    booking_reference,
-    bill_of_lading_number,
-    equipment_reference
-)
-    VALUES ('http://172.17.0.1:4567/webhook/receive', '', '', '', '');
-
-
-INSERT INTO dcsa_v2_0.shipment(
-    shipment_id,
+INSERT INTO dcsa_v2_0.shipment (
+    id,
     booking_reference,
     booking_datetime,
     transport_document_id,
@@ -178,7 +239,7 @@ INSERT INTO dcsa_v2_0.shipment(
     uuid('6e2d856c-d871-11ea-a630-03e5334d1800'),
     'BR1239719871',
     DATE '2020-03-07',
-    'BL32147109',
+    uuid('6e2d856c-d871-11ea-a630-03e5334d1811'),
     'BOL',
     'Magic Wands Inc.',
     'The Mage Guild',
@@ -186,57 +247,5 @@ INSERT INTO dcsa_v2_0.shipment(
     DATE '2020-03-10',
     '125, Valley Street, The land nearby',
     DATE '2020-04-10',
-    'SMDG-ZIM'
-);
-
-INSERT INTO dcsa_v1_1.shipment(
-    shipment_id,
-    booking_reference,
-    booking_datetime,
-    blnumber,
-    shipper_name,
-    consignee_name,
-    collection_origin,
-    collection_dateTime,
-    delivery_destination,
-    delivery_datetime,
-    carrier_code
-) VALUES (
-    uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'),
-    'BR1239719871',
-    DATE '2020-03-07',
-    'BL32147109',
-    'Magic Wands Inc.',
-    'The Mage Guild',
-    '5, Mountain Road, The land beyond the sea',
-    DATE '2020-03-10',
-    '125, Valley Street, The land nearby',
-    DATE '2020-04-10',
-    'SMDG-ZIM'
-);
-
-INSERT INTO dcsa_v1_1.shipment(
-    shipment_id,
-    booking_reference,
-    booking_datetime,
-    blnumber,
-    shipper_name,
-    consignee_name,
-    collection_origin,
-    collection_dateTime,
-    delivery_destination,
-    delivery_datetime,
-    carrier_code
-) VALUES (
-    uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea2'),
-    'BR1239719872',
-    DATE '2021-03-07',
-    'AA32147122',
-    'Unicorn Inc.',
-    'Long horn',
-    '5, Lake View, By the lake',
-    DATE '2021-05-23',
-    '125, Wall St, Next door',
-    DATE '2020-08-01',
     'SMDG-ZIM'
 );
