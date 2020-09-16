e6f1e3ee7ad7189d8327a508a05f8cae11e54170
Henrik Helmø Larsen <hhl@asseco.dk>
2020-09-16 15:26:33 +0200

    Update schema for queries

--- src/main/java/org/dcsa/tnt/repository/TransportEquipmentEventRepository.java
+++ src/main/java/org/dcsa/tnt/repository/TransportEquipmentEventRepository.java
@@ -11,6 +11,6 @@ import java.util.UUID;
 
 public interface TransportEquipmentEventRepository extends ExtendedRepository<TransportEquipmentEvent, UUID> {
 
-    @Query("SELECT * FROM \"dcsa_v1_1\".transport_equipment_event a WHERE (:eventType IS NULL or a.event_type =:eventType) AND (:equipmentReference IS NULL or a.equipment_reference =:equipmentReference) ")
+    @Query("SELECT * FROM \"dcsa_v1_2\".transport_equipment_event a WHERE (:eventType IS NULL or a.event_type =:eventType) AND (:equipmentReference IS NULL or a.equipment_reference =:equipmentReference) ")
     Flux<TransportEquipmentEvent> findTransportEquipmentEventsByFilters(@Param("eventType") EventType eventType, String bookingReference, @Param("equipmentReference") String equipmentReference);
 }
