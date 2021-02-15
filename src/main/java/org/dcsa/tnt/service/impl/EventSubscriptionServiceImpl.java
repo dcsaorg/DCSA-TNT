6661ec379481bf049b155a2c1983320f4862973f
Niels Thykier <nt@asseco.dk>
2021-02-15 11:27:07 +0100

    EventSubscriptionService: Validate URLs before storing them
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/tnt/service/impl/EventSubscriptionServiceImpl.java
+++ src/main/java/org/dcsa/tnt/service/impl/EventSubscriptionServiceImpl.java
@@ -1,12 +1,16 @@
 package org.dcsa.tnt.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.dcsa.core.exception.UpdateException;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.tnt.model.EventSubscription;
 import org.dcsa.tnt.repository.EventSubscriptionRepository;
 import org.dcsa.tnt.service.EventSubscriptionService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,5 +26,16 @@ public class EventSubscriptionServiceImpl extends ExtendedBaseServiceImpl<EventS
     @Override
     public Class<EventSubscription> getModelClass() {
         return EventSubscription.class;
+    }
+
+    @Override
+    protected Mono<EventSubscription> preSaveHook(EventSubscription eventSubscription) {
+        // Ensure that the callback url at least looks valid.
+        try {
+            new URI(eventSubscription.getCallbackUrl());
+        } catch (URISyntaxException e) {
+            return Mono.error(new UpdateException("callbackUrl is invalid: " + e.getLocalizedMessage()));
+        }
+        return super.preSaveHook(eventSubscription);
     }
 }
