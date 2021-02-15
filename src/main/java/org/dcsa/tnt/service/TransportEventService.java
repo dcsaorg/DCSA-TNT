e4228fbffec41dcdb4bab18c03c90838f01581f4
Niels Thykier <nt@asseco.dk>
2021-02-15 13:15:52 +0100

    Remove unused code
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/tnt/service/TransportEventService.java
+++ src/main/java/org/dcsa/tnt/service/TransportEventService.java
@@ -2,12 +2,8 @@ package org.dcsa.tnt.service;
 
 import org.dcsa.core.service.ExtendedBaseService;
 import org.dcsa.tnt.model.TransportEvent;
-import org.dcsa.tnt.model.enums.EventType;
-import reactor.core.publisher.Flux;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface TransportEventService extends ExtendedBaseService<TransportEvent, UUID> {
-    Flux<TransportEvent> findTransportEvents(List<EventType> eventType, String bookingReference, String equipmentReference);
 }
--- src/main/java/org/dcsa/tnt/service/impl/ShipmentEventServiceImpl.java
+++ src/main/java/org/dcsa/tnt/service/impl/ShipmentEventServiceImpl.java
@@ -3,14 +3,11 @@ package org.dcsa.tnt.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.tnt.model.ShipmentEvent;
-import org.dcsa.tnt.model.enums.EventType;
 import org.dcsa.tnt.repository.ShipmentEventRepository;
 import org.dcsa.tnt.service.ShipmentEventService;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.UUID;
 
 
@@ -33,15 +30,5 @@ public class ShipmentEventServiceImpl extends ExtendedBaseServiceImpl<ShipmentEv
     @Override
     public Mono<ShipmentEvent> findById(UUID id) {
         return getRepository().findById(id);
-    }
-
-    public Flux<ShipmentEvent> findShipmentEvents(List<EventType> eventType, String bookingReference, String equipmentReference) {
-        // Return empty if SHIPMENT event type is not defined
-        if (!eventType.contains(EventType.SHIPMENT)) return Flux.empty();
-        // If bookingReference is defined, we return empty - since bookingReferences don't exist in shipmentEvents
-        if (bookingReference!=null ) return Flux.empty();
-        // If equipmentReference is defined, we return empty - since equipmentReferences don't exist in shipmentEvents
-        if (equipmentReference!=null ) return Flux.empty();
-        return shipmentEventRepository.findShipmentEventsByFilters(EventType.SHIPMENT);
     }
 }
