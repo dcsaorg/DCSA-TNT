4fc92c73343b1edbeb7e92a1cd5bc8733bc147d8
Niels Thykier <nt@asseco.dk>
2021-04-27 11:09:11 +0200

    Update SecurityConfig to support disabling security
    Backport from the DCSA-Core.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/tnt/security/SecurityConfig.java
+++ src/main/java/org/dcsa/tnt/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.dcsa.tnt.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -17,6 +18,7 @@ import security.JwtAuthenticationConverter;
 /**
  * Configures our application with Spring Security to restrict access to our API endpoints.
  */
+@Slf4j
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
@@ -25,6 +27,9 @@ public class SecurityConfig {
 
     @Value("${auth0.audience}")
     private String audience;
+
+    @Value("${auth0.enabled:true}")
+    private boolean securityEnabled;
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
@@ -37,46 +42,38 @@ public class SecurityConfig {
         return source;
     }
 
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         /*
         This is where we configure the security required for our endpoints and setup our app to serve as
         an OAuth2 Resource Server, using JWT validation.
         */
-        return http
-                .authorizeExchange()
-                .anyExchange().authenticated()
+
+        ServerHttpSecurity.AuthorizeExchangeSpec securitySpec = http.authorizeExchange();
+
+        if (securityEnabled) {
+            log.info("Security: auth0 enabled");
+            securitySpec
+                    .anyExchange().authenticated()
 //                .pathMatchers("/*").permitAll()
 //                .pathMatchers("/*").authenticated()
 //                .pathMatchers("/*").hasAuthority("readwrite:all")
 //                .anyExchange().authenticated()
-                .and()
-                .cors()
-                .and()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(new JwtAuthenticationConverter())
-                .and()
+                    .and()
+                    .cors()
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt()
+                    .jwtAuthenticationConverter(new JwtAuthenticationConverter());
+        } else {
+            log.info("Security: disabled - no authentication nor CRSF tokens needed");
+            securitySpec.anyExchange().permitAll()
+                    .and()
+                    .csrf().disable();
+        }
+        return securitySpec
                 .and()
                 .build();
-    }
-
-    @Bean
-    ReactiveJwtDecoder jwtDecoder() {
-        /*
-        By default, Spring Security does not validate the "aud" claim of the token, to ensure that this token is
-        indeed intended for our app. Adding our own validator is easy to do:
-        */
-
-        NimbusReactiveJwtDecoder jwtDecoder = (NimbusReactiveJwtDecoder)
-                ReactiveJwtDecoders.fromOidcIssuerLocation(issuer);
-
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator,
-                new JwtTimestampValidator());
-
-        jwtDecoder.setJwtValidator(withAudience);
-        return jwtDecoder;
     }
 }
