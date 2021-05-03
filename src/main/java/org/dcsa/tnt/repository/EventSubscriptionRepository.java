94070e2a9376ab15fffb65d1386ed98d68ead32d
Niels Thykier <nt@asseco.dk>
2021-05-03 16:26:25 +0200

    Make TNT eventType filers work
    Closes: #23
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/tnt/repository/EventSubscriptionRepository.java
+++ src/main/java/org/dcsa/tnt/repository/EventSubscriptionRepository.java
@@ -2,15 +2,41 @@ package org.dcsa.tnt.repository;
 
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.tnt.model.EventSubscription;
+import org.dcsa.tnt.model.EventSubscriptionEventType;
 import org.dcsa.tnt.model.enums.EventType;
+import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EventSubscriptionRepository extends ExtendedRepository<EventSubscription, UUID> {
 
-    @Query("SELECT callback_url FROM event_subscription a WHERE ((:eventType IS NULL or a.event_type =:eventType) AND (:equipmentReference IS NULL or a.equipment_reference =:equipmentReference)) OR a.event_type = ''")
+    @Query("SELECT callback_url FROM event_subscription es"
+            + "  JOIN event_subscription_event_types eset ON (es.subscription_id = eset.subscription_id)"
+            + " WHERE eset.event_type = :eventType AND (:equipmentReference IS NULL or es.equipment_reference = :equipmentReference)")
     Flux<String> findSubscriptionsByFilters(@Param("eventType") EventType eventType, @Param("equipmentReference") String equipmentReference);
+
+    @Modifying
+    @Query("INSERT INTO event_subscription_event_types (subscription_id, event_type)"
+            + " VALUES (:subscriptionID, :eventType)")
+    Mono<Void> insertEventTypeForSubscription(UUID subscriptionID, EventType eventType);
+
+    @Query("SELECT event_type FROM event_subscription_event_types"
+            + " WHERE subscription_id = :subscriptionID")
+    Flux<EventType> findEventTypesForSubscription(UUID subscriptionID);
+
+
+    @Modifying
+    @Query("DELETE FROM event_subscription_event_types WHERE subscription_id = :subscriptionID")
+    Mono<Void> deleteEventTypesForSubscription(UUID subscriptionID);
+
+    @Query("SELECT event_subscription_event_types.* FROM event_subscription_event_types"
+            + " WHERE subscription_id IN (:subscriptionIDs)"
+            + " ORDER BY event_subscription_id, event_type"
+    )
+    Flux<EventSubscriptionEventType> findEventTypesForSubscriptionIDIn(List<UUID> subscriptionIDs);
 }
