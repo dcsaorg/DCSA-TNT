2510b9cf060e8fcbd8a13a96697e99f93e9e3d57
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-06-10 11:43:28 +0200

    Removed swagger annotations and dependency
    Resolves:
    https://github.com/dcsaorg/DCSA-TNT/issues/43#issue-917137852

--- src/main/java/org/dcsa/tnt/Application.java
+++ src/main/java/org/dcsa/tnt/Application.java
@@ -1,12 +1,8 @@
 package org.dcsa.tnt;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.dcsa.core.repository.ExtendedRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
@@ -14,14 +10,6 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 @ComponentScan("org.dcsa")
 @EnableR2dbcRepositories(basePackages = {"org.dcsa"}, repositoryBaseClass = ExtendedRepositoryImpl.class)
 public class Application {
-
-	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-				.components(new Components())
-				.info(new Info().title("DCSA API").description(
-						"This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3.").version("1.0"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
