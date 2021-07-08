095d78a77ac8b77cc98e49c1ebd24fb83f746b74
abf <32563466+Abdiiir@users.noreply.github.com>
2021-07-08 17:54:11 +0200

    Revert "Merge branch 'master' into ddt-29"
    This reverts commit 9fa8bd9702d6409b13c0ea36c1ae658df9b3233b, reversing
    changes made to 7b0b2aa779817f67b00c17272e8a750b48e671ae.

--- src/main/java/org/dcsa/tnt/controller/EventController.java
+++ src/main/java/org/dcsa/tnt/controller/EventController.java
@@ -6,14 +6,9 @@ import org.dcsa.core.events.model.Event;
 import org.dcsa.core.events.util.ExtendedGenericEventRequest;
 import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.dcsa.tnt.service.EventService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Mono;
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,10 +25,5 @@ public class EventController extends AbstractEventController<EventService, Event
     @Override
     protected ExtendedRequest<Event> newExtendedRequest() {
         return new ExtendedGenericEventRequest(extendedParameters, r2dbcDialect);
-    }
-
-    @Override
-    public Mono<Event> create(@Valid @RequestBody Event event) {
-        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
     }
 }
