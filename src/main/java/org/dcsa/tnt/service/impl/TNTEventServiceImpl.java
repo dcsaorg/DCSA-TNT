76e1f7b613389c4a3b0330206f41f61b27dca245
Martin Petersson <mp@asseco.dk>
2021-08-02 11:15:09 +0200

    Override of GenericEvent implementation

--- src/main/java/org/dcsa/tnt/service/impl/TNTEventServiceImpl.java
+++ src/main/java/org/dcsa/tnt/service/impl/TNTEventServiceImpl.java
@@ -1,22 +1,17 @@
 package org.dcsa.tnt.service.impl;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.dcsa.core.events.model.EquipmentEvent;
 import org.dcsa.core.events.model.Event;
 import org.dcsa.core.events.model.ShipmentEvent;
 import org.dcsa.core.events.model.TransportEvent;
 import org.dcsa.core.events.repository.EventRepository;
 import org.dcsa.core.events.repository.PendingEventRepository;
-import org.dcsa.core.events.service.EquipmentEventService;
-import org.dcsa.core.events.service.GenericEventService;
-import org.dcsa.core.events.service.ShipmentEventService;
-import org.dcsa.core.events.service.TransportEventService;
+import org.dcsa.core.events.service.*;
 import org.dcsa.core.events.service.impl.GenericEventServiceImpl;
 import org.dcsa.core.exception.NotFoundException;
 import org.dcsa.core.extendedrequest.ExtendedRequest;
-import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.tnt.service.TNTEventService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,11 +19,47 @@ import reactor.core.publisher.Mono;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-@RequiredArgsConstructor
+@Slf4j
 @Service
 public class TNTEventServiceImpl extends GenericEventServiceImpl implements TNTEventService {
 
+    private final TransportEventService transportEventService;
+    private final EquipmentEventService equipmentEventService;
+    private final ShipmentEventService shipmentEventService;
     private final PendingEventRepository pendingEventRepository;
+
+    public TNTEventServiceImpl(TransportEventService transportEventService, EquipmentEventService equipmentEventService, ShipmentEventService shipmentEventService, OperationsEventService operationsEventService, EventRepository eventRepository, PendingEventRepository pendingEventRepository) {
+        super(shipmentEventService, transportEventService, equipmentEventService,operationsEventService, eventRepository);
+        this.transportEventService = transportEventService;
+        this.equipmentEventService = equipmentEventService;
+        this.shipmentEventService = shipmentEventService;
+        this.pendingEventRepository = pendingEventRepository;
+    }
+
+    @Override
+    public Flux<Event> findAllExtended(ExtendedRequest<Event> extendedRequest) {
+        return super.findAllExtended(extendedRequest).concatMap(event -> {
+            switch (event.getEventType()) {
+                case TRANSPORT:
+                    return transportEventService.loadRelatedEntities((TransportEvent) event);
+                case EQUIPMENT:
+                    return equipmentEventService.loadRelatedEntities((EquipmentEvent) event);
+                case SHIPMENT:
+                    return shipmentEventService.loadRelatedEntities((ShipmentEvent) event);
+                default:
+                    return Mono.just(event);
+            }
+        });
+    }
+
+    @Override
+    public Mono<Event> findById(UUID id) {
+        return Mono.<Event>empty()
+                .switchIfEmpty(getTransportEventRelatedEntities(id))
+                .switchIfEmpty(getShipmentEventRelatedEntities(id))
+                .switchIfEmpty(getEquipmentEventRelatedEntities(id))
+                .switchIfEmpty(Mono.error(new NotFoundException("No event was found with id: " + id)));
+    }
 
     @Override
     public Mono<Event> create(Event event) {
