8e9d3451d28cba32c908d44185f4ad3518d9c413
Martin Petersson <mp@asseco.dk>
2021-08-02 14:59:05 +0200

    Filter Event types for TNT on findAllExtended

--- src/main/java/org/dcsa/tnt/service/impl/TNTEventServiceImpl.java
+++ src/main/java/org/dcsa/tnt/service/impl/TNTEventServiceImpl.java
@@ -47,7 +47,7 @@ public class TNTEventServiceImpl extends GenericEventServiceImpl implements TNTE
                 case SHIPMENT:
                     return shipmentEventService.loadRelatedEntities((ShipmentEvent) event);
                 default:
-                    return Mono.just(event);
+                    return Mono.empty();
             }
         });
     }
