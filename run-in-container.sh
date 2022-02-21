ef290f5cd587c540f87559bbd8bf9251b2fb2118
Michael Wraa Laustsen <mwl@asseco.dk>
2022-02-21 09:13:39 +0100

    Ddt 867 (#79)
    * DDT-867 Add Url prefix env variable
    
    To be able to route calls to tnt in sandbox projectt
    
    * DDT-867 Set ENV properties in dcsa-config.yaml

---
+++ run-in-container.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "spring.config.import=/application.yaml" >> dcsa-spring-boot.properties
+
+if [ -f "/config/dcsa-config.yaml" ]; then
+  echo "Add /config/dcsa-config.yaml to dcsa-spring-boot.properties"
+  echo "#---" >> dcsa-spring-boot.properties
+  echo "spring.config.import=/config/dcsa-config.yaml" >> dcsa-spring-boot.properties
+else
+  echo "No custom config.  Add a /config/dcsa-config.yaml if you want to set custom properties"
+fi
+
+exec java -Dspring.config.location=/dcsa-spring-boot.properties -jar dcsa*.jar
