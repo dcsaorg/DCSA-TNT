d4cd13faa590bf2a30e564a481a4f9639d71c71c
abf <abf@asseco.dk>
2022-10-18 13:43:28 +0200

    feedback: add add view_id to reference view & more
    - class renamed to EventReference
    - viewID is added to satisfy JPA primary key requirement

--- tnt-persistence/src/main/java/org/dcsa/tnt/persistence/repository/ReferenceRepository.java
+++ tnt-persistence/src/main/java/org/dcsa/tnt/persistence/repository/ReferenceRepository.java
@@ -1,6 +1,6 @@
 package org.dcsa.tnt.persistence.repository;
 
-import org.dcsa.tnt.persistence.entity.Reference;
+import org.dcsa.tnt.persistence.entity.EventReference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,10 +9,10 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ReferenceRepository extends JpaRepository<Reference, UUID>, JpaSpecificationExecutor<Reference> {
-  List<Reference> findDocumentReferencesByTransportCallID(UUID transportCallID);
-  List<Reference> findReferencesByUtilizedEquipmentID(UUID utilizedEquipmentID);
-  List<Reference> findDocumentReferencesByLinkTypeAndDocumentID(
+public interface ReferenceRepository extends JpaRepository<EventReference, UUID>, JpaSpecificationExecutor<EventReference> {
+  List<EventReference> findDocumentReferencesByTransportCallID(UUID transportCallID);
+  List<EventReference> findReferencesByUtilizedEquipmentID(UUID utilizedEquipmentID);
+  List<EventReference> findDocumentReferencesByLinkTypeAndDocumentID(
     String documentReferenceType, UUID documentID);
 }
 
