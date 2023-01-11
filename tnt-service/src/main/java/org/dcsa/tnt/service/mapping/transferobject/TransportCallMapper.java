9bc63bdfcbb8ff94b6a612324976ffc9ec8ec1af
abf <abf@asseco.dk>
2023-01-11 10:15:58 +0100

    DDT-1419: map modeOfTransport in transportCall

--- tnt-service/src/main/java/org/dcsa/tnt/service/mapping/transferobject/TransportCallMapper.java
+++ tnt-service/src/main/java/org/dcsa/tnt/service/mapping/transferobject/TransportCallMapper.java
@@ -1,9 +1,15 @@
 package org.dcsa.tnt.service.mapping.transferobject;
 
+import org.dcsa.skernel.errors.exceptions.ConcreteRequestErrorMessageException;
+import org.dcsa.tnt.persistence.entity.enums.DCSATransportType;
 import org.dcsa.tnt.service.domain.TransportCall;
+import org.dcsa.tnt.transferobjects.ModeOfTransport;
 import org.dcsa.tnt.transferobjects.TransportCallTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Arrays;
 
 @Mapper(
   componentModel = "spring",
@@ -19,5 +25,15 @@ public interface TransportCallMapper {
   @Mapping(target = "universalImportVoyageReference", source = "importVoyage.universalVoyageReference")
   @Mapping(target = "carrierServiceCode", source = "exportVoyage.service.carrierServiceCode")
   @Mapping(target = "universalServiceReference", source = "importVoyage.service.universalServiceReference")
+  @Mapping(target = "modeOfTransport",source = "modeOfTransportCode", qualifiedByName = "modeOfTransportCodetoTO")
   TransportCallTO toDTO(TransportCall transportCall);
+  @Named("modeOfTransportCodetoTO")
+  default ModeOfTransport modeOfTransportCodetoTO(String modeOfTransportCode) {
+    return Arrays.stream(DCSATransportType.values())
+      .filter(t -> t.getCode().toString().equals(modeOfTransportCode))
+      .findFirst()
+      .map(e -> ModeOfTransport.valueOf(e.name()))
+      .orElseThrow(() -> ConcreteRequestErrorMessageException.invalidInput(modeOfTransportCode + " is not a valid transport code"));
+  }
+
 }
---
+++ tnt-transfer-obj/src/main/java/org/dcsa/tnt/transferobjects/ModeOfTransport.java
@@ -0,0 +1,13 @@
+package org.dcsa.tnt.transferobjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ModeOfTransport {
+  VESSEL,
+  RAIL,
+  TRUCK,
+  BARGE
+}
